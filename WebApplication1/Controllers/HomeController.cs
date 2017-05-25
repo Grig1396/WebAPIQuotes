3cc9155cb5b1e5f1ae4ac48fcb39eecbb4881572
KameKAgZZe <kamekagzze@mail.ru>
2017-05-25 15:00:30 +0600

    tableToExcel

--- WebApplication1/Controllers/HomeController.cs
+++ WebApplication1/Controllers/HomeController.cs
@@ -28,21 +28,27 @@ public class HomeController : Controller
             var JsonQuote = ParseQuote(character);
             ViewBag.Quote = JsonQuote.Quote;
             ViewBag.Character = JsonQuote.Character;
-            string jsonResponse = Yodafication(ViewBag.Quote);
-            var Numbers = NumbersFact(RandomType(), jsonResponse.Length);
+            var jsonResponse = Yodafication(ViewBag.Quote);
+        string YodaQuote = Regex.Replace(jsonResponse.Body, "[ ]+", " ");
+        if (jsonResponse.Code != 200)
+        {
+            ViewBag.Message = "Сломался сервер Yoda Speak. Мы не виноваты.";
+            return PartialView();
+        }
+            var Numbers = NumbersFact(RandomType(), YodaQuote.Length);
             Dictionary<int, string> result = new Dictionary<int, string>();
             List<string> typeList = new List<string>();
             int i = 0;
-            for (int k = 0; k < jsonResponse.Length-1; k++)
+            for (int k = 0; k < YodaQuote.Length-1; k++)
             {
-                if (vowels.Contains(jsonResponse[k]))
+                if (vowels.Contains(YodaQuote[k]))
                 {
                     i++;
                     typeList.Add(RandomType());
                     result.Add(i * Numbers.number, NumbersFact(typeList[i - 1], (i * Numbers.number)).text);
                 }
             }
-            ViewBag.Message = jsonResponse;
+            ViewBag.Message = YodaQuote;
             ViewBag.text = Numbers.text;
             ViewBag.type = Numbers.type;
             ViewBag.number = Numbers.number;
@@ -52,20 +58,18 @@ public class HomeController : Controller
         }
         public dynamic ParseQuote(string character) 
         {
-            HttpResponse<string> response = Unirest.get("https://got-quotes.herokuapp.com/quotes?char="+character)
-            .asJson<string>();
-            return JsonConvert.DeserializeObject<QuoteObject>(response.Body);
-        
-        
-        }
+        HttpResponse<string> response = Unirest.get("https://got-quotes.herokuapp.com/quotes?char=" + character)
+        .asJson<string>();
+        return JsonConvert.DeserializeObject<QuoteObject>(response.Body);
+    }
 
-        public string Yodafication(string quote)
+        public HttpResponse<string> Yodafication(string quote)
         {
         // HttpResponse<string> jsonResponse = Unirest.get("https://yoda.p.mashape.com/yoda?sentence=" + quote)
             HttpResponse<string> jsonResponse = Unirest.get("https://yoda.p.mashape.com/yoda?sentence=" + quote)
             .header("X-Mashape-Authorization", "ejWTgwZXAlmshfC6wOWKifd9Am2Lp1OrhYkjsnfOF0oNRh1L9f")
             .asJson<string>();
-            return Regex.Replace(jsonResponse.Body, "[ ]+", " ");
+        return jsonResponse;
         }
 
         public dynamic NumbersFact(string type, int number)
